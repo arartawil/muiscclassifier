a1624271cd2442067cd460c24e2ddbb6eac0ba10
arartawil <arartawil@yahoo.com>
2021-11-09 17:43:02 +0200

    SETTING
    SETTING

--- myweb/settings.py
+++ myweb/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ciud64o*8!3_hjrex*3cp(zmm3i9+v@68=j6zo1d9(4$ki^+t('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://muiscclassifier.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
